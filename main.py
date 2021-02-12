ab045ad336e39e32ec8ab68d93e47d0d3929d10d
oopayne <leftydoubleop@gmail.com>
2021-02-12 12:08:56 -0600

    added procedurally generated dungeon

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
+from procgen import generate_dungeon
 from input_handlers import EventHandler
 
 def main() -> None:
@@ -12,6 +12,10 @@ def main() -> None:
 
     map_width = 80
     map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -23,7 +27,14 @@ def main() -> None:
     npc = Entity(int(screen_width/2 - 5), int(screen_height/2), "@", (255, 255, 0))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map,  player=player)
 
