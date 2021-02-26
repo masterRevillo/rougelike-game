b14d15ea7eb54031c1cbffba499d52480ea93cfd
oopayne <leftydoubleop@gmail.com>
2021-02-25 22:18:52 -0600

    add increasing difficulty

--- game_map.py
+++ game_map.py
@@ -107,8 +107,6 @@ class GameWorld:
             max_rooms: int,
             room_max_size: int,
             room_min_size: int,
-            max_monsters_per_room: int,
-            max_items_per_room: int,
             current_floor: int = 0
     ):
         self.engine = engine
@@ -117,8 +115,6 @@ class GameWorld:
         self.max_rooms = max_rooms
         self.room_max_size = room_max_size
         self.room_min_size = room_min_size
-        self.max_monsters_per_room = max_monsters_per_room
-        self.max_items_per_room = max_items_per_room
         self.current_floor = current_floor
 
     def generate_floor(self) -> None:
@@ -132,7 +128,5 @@ class GameWorld:
             room_min_size=self.room_min_size,
             map_width=self.map_width,
             map_height=self.map_height,
-            max_monsters_per_room=self.max_monsters_per_room,
-            max_items_per_room=self.max_items_per_room,
             engine=self.engine
         )
